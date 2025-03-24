41255566cbf4003fef0a02921cda8576bcfbe0ec
Cameron <cporter@us.ci.org>
2025-03-24 14:10:01 -0600

    fetch and display movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,27 @@ import SearchBar from '@/components/SearchBar';
 import { icons } from '@/constants/icons';
 import { images } from '@/constants/images';
 import React from 'react';
-import { Text, View, Image, ScrollView } from 'react-native';
+import {
+  Text,
+  View,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 import { useRouter } from 'expo-router';
+import useFetch from '@/services/useFetch';
+import { fetchMovies } from '@/services/api';
+import MovieCard from '@/components/MovieCard';
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: '' }));
 
   return (
     <View className='flex-1 bg-primary'>
@@ -26,12 +42,41 @@ export default function Index() {
           source={icons.logo}
           className='w-12 h-10 mt-20 mb-5 mx-auto'
         />
-        <View className='flex-1 mt-5'>
-          <SearchBar
-            onPress={() => router.push('/search')}
-            placeholder='Search for a movie'
+        {moviesLoading ? (
+          <ActivityIndicator
+            size='large'
+            color='#0000ff'
+            className='mt-10 self-center'
           />
-        </View>
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
+          <View className='flex-1 mt-5'>
+            <SearchBar
+              onPress={() => router.push('/search')}
+              placeholder='Search for a movie'
+            />
+            <>
+              <Text className='text-lg text-white font-bold mt-5 mb-3'>
+                Latest Movies
+              </Text>
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: 'flex-start',
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                className='mt-2 pb-32'
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
